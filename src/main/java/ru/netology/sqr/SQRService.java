90b397cb3c30ff9f08b4f55b7d09cd51e243021d
Igor Meleshko <igor.meleshko.1109@gmail.com>
2021-11-24 23:01:42 +0300

    commit NomberOfSquares

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,15 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int enumeration(int min, int max) {
+        int num;
+        int count = 0;
+        for (int i = 10; i <= 99; i++) {
+            num = i * i;
+            if (num >= min && num <= max) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
